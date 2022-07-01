243cca8865a8f20cedff0618ce30aae2b5d926a9
Michel Correa <michelcm13@gmail.com>
2022-07-01 08:20:49 -0300

    menu active completed

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,25 +1,51 @@
 import { Request, Response } from 'express';
 
+import { createActiveMenuObject } from '../helpers/createActiveMenuObject';
+
 export const home = (req: Request, res: Response) => {
 
-    res.render('pages/page');
+    res.render('pages/page', {
+        menu: createActiveMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 
 }
 
 export const dogs = (req: Request, res: Response) => {
 
-    res.send('dogs');
+    res.render('pages/page', {
+        menu: createActiveMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
 
 }
 
 export const cats = (req: Request, res: Response) => {
 
-    res.send('cats');
+    res.render('pages/page', {
+        menu: createActiveMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 
 }
 
 export const fishes = (req: Request, res: Response) => {
 
-    res.send('fishes');
+    res.render('pages/page', {
+        menu: createActiveMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
 
 }
---
+++ src/helpers/createActiveMenuObject.ts
@@ -0,0 +1,17 @@
+type MenuOptions = '' | 'all' | 'dog' | 'cat' | 'fish';
+
+export const createActiveMenuObject = (activeMenu: MenuOptions) => {
+    let returnObject = {
+        all: false,
+        dog: false,
+        cat: false,
+        fish: false
+    };
+
+    if(activeMenu !== '') {
+        returnObject[activeMenu] = true;
+    }
+
+    return returnObject;
+
+}
